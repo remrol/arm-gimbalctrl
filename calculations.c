6d02676ebb95920f9464eff560ac9e8ad4a7ba0f
Remigiusz Zukowski <remigiusz.zukowski@avid.com>
2018-02-09 08:58:09 +0100

    expo cont.

--- calculations.c
+++ calculations.c
@@ -10,9 +10,7 @@ uint16_t expo (uint16_t pulse_time_ms, uint8_t percent /* 0-100 */)
 	int32_t x = ((int32_t) pulse_time_ms) * X_RANGE / ( ( PULSE_MAX_MS - PULSE_MIN_MS ) / 2 ); 
 	
 	// all calculations from this point on are signed 10 bits:
-	x = (   x * x * x / 65536 * percent / ( X_RANGE * X_RANGE / 65536 ) +
-			( P_RANGE - percent ) * x + 
-			P_RANGE / 2 ) / P_RANGE;
+	x = (   x * x * x / 65536 * percent / ( X_RANGE * X_RANGE / 65536 ) + ( P_RANGE - percent ) * x +  P_RANGE / 2 ) / P_RANGE;
 	
 	// now scale it back up to full servo pulse:
 	x = x * ( ( PULSE_MAX_MS - PULSE_MIN_MS ) / 2 ) / X_RANGE;
